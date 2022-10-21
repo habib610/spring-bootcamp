9ba8c073c9d3873d4b3d73b97b4571025d1e36da
habib610 <habib54562@gmail.com>
2022-10-21 22:23:42 +0600

    startup-spring-boot

---
+++ completespring/src/main/java/com/rhabib/completespring/CompletespringApplication.java
@@ -0,0 +1,13 @@
+package com.rhabib.completespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompletespringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompletespringApplication.class, args);
+	}
+
+}
