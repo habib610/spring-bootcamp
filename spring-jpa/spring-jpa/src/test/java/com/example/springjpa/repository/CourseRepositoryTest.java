6937e2363e353a50d02abc9c891cd7f7de9396f4
habib610 <habib54562@gmail.com>
2022-10-31 19:16:02 +0600

    PAGING-AND-SORTING

--- spring-jpa/spring-jpa/src/test/java/com/example/springjpa/repository/CourseRepositoryTest.java
+++ spring-jpa/spring-jpa/src/test/java/com/example/springjpa/repository/CourseRepositoryTest.java
@@ -1,28 +1,30 @@
 package com.example.springjpa.repository;
 
 import com.example.springjpa.entities.Course;
-import com.example.springjpa.entities.CourseMaterial;
 import com.example.springjpa.entities.Teacher;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class CourseRepositoryTest {
 
     @Autowired
     private CourseRepository courseRepository;
+
     @Test
-    public void printCourseList(){
+    public void printCourseList() {
         List<Course> courses = courseRepository.findAll();
         System.out.println("Couses++++++++++++++++++++++>>>>>>>>>>>");
         System.out.println(courses);
     }
-@Test
-    public void saveCourseWithTeacher(){
+
+    @Test
+    public void saveCourseWithTeacher() {
         Teacher teacher = Teacher.builder()
                 .firstName("RahaNahiJata")
                 .lastName("Last Name")
@@ -37,7 +39,19 @@ class CourseRepositoryTest {
     }
 
     @Test
-    public void findAllPagination(){
+    public void findAllPagination() {
+        Pageable firstPageWithThreeRecords =
+                PageRequest.of(0, 3);
+        Pageable secondPageWithTwoRecords =
+               PageRequest.of(1, 2);
+
+        List<Course> courses = courseRepository.findAll(firstPageWithThreeRecords).getContent();
+        long coursesElement = courseRepository.findAll(firstPageWithThreeRecords).getTotalElements();
+        long totalPages = courseRepository.findAll(firstPageWithThreeRecords).getTotalPages();
+        System.out.println("Coursess=======================>>>>>>>>");
+        System.out.println("totalPages:" + totalPages);
+        System.out.println("coursesElement:" + coursesElement);
+        System.out.println("courses:" + courses);
 
     }
 }
